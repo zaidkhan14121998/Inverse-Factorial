41ad941d377dac9cc285a77274307a11aeadf7de
Mohammad Zaid <100582667+zaidkhan14121998@users.noreply.github.com>
2023-02-15 13:11:15 +0530

    Add files via upload

---
+++ check age .py
@@ -0,0 +1,24 @@
+name=input("Enter a Name : ")
+x=int(input("Enter a birth year : "))
+y=int(input("Enter a current year : "))
+
+a=0
+
+if x<0:
+
+    print("enter a positiver year")
+
+else :
+    a=y-x
+
+    c=a*365
+
+# print(f"{name} is {a} Year old.")
+print(f"{name} is {c} dayssss old.")
+
+
+
+
+
+
+
